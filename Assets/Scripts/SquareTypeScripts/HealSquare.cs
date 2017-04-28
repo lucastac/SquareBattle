125045d6e794d52ee656d1a205c7005d4058afa8
Lucas <lucas_tac18@hotmail.com>
2017-04-28 00:40:47 -0300

    Some little stuffs, just to backup an updated version

---
+++ Assets/Scripts/SquareTypeScripts/HealSquare.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealSquare : GeneralSquare {
+
+    public int number_of_heal_targets = 1;
+
+    public override void Act()
+    {
+        if (level == 3)
+            myPlayer.MainSquare.Heal(attack);
+
+
+        for (int j = 0; j < number_of_heal_targets; j++)
+        {
+            int i;
+            do
+            {
+                i = Random.Range(0, myPlayer.mySquares.Count);
+            } while (myPlayer.mySquares[i].gameObject.activeInHierarchy);
+
+            myPlayer.mySquares[i].Heal(attack);
+        }
+    }
+
+    public override void ResetStatus()
+    {
+        base.ResetStatus();
+        number_of_heal_targets = 1;
+    }
+
+    protected override void LevelUp()
+    {
+        number_of_heal_targets++;
+        attack++;
+        base.LevelUp();
+    }
+}
